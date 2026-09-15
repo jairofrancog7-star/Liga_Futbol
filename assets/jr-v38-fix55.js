f94a158461d8d00268c8dfc8983bba317c0cc8ca
Jairo Franco Garduño <jairofrancog7@gmail.com>
2026-09-15 05:08:45 -0600

    fix: repair broken FIX55 loader and hand off to FIX56

--- assets/jr-v38-fix55.js
+++ assets/jr-v38-fix55.js
@@ -1,1 +1,6 @@
-cat: assets/jr-v38-fix55.js: No such file or directory
+/* FIX55 compatibility loader -> FIX56 */
+(()=>{if(window.__JR55Compat56)return;window.__JR55Compat56=true;
+const v='38-56-r1';
+if(!document.querySelector('link[data-jr56]')){const l=document.createElement('link');l.rel='stylesheet';l.href='./assets/jr-v38-fix56.css?v='+v;l.dataset.jr56='1';document.head.appendChild(l)}
+if(!document.querySelector('script[data-jr56]')){const s=document.createElement('script');s.src='./assets/jr-v38-fix56.js?v='+v;s.defer=true;s.dataset.jr56='1';document.head.appendChild(s)}
+})();
